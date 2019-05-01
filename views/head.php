3a36e013c1954cb7340f5a63b5647d088c7af428
david reinfurt <reinfurt@o-r-g.com>
2019-05-01 17:52:27 -0400

    update name in webapp

--- views/head.php
+++ views/head.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-title" content="serverdial">
+        <meta name="apple-mobile-web-app-title" content="nyc-led">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <link rel="apple-touch-icon" href="./media/png/apple-touch-icon.png" />
         <link rel="stylesheet" href="./static/css/main.css">
