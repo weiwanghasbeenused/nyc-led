567967128165dfa160d94f7e486fda7b1a517be9
david reinfurt <reinfurt@o-r-g.com>
2019-05-01 17:50:43 -0400

    add dirs

---
+++ views/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>New York Consolidated</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="serverdial">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <link rel="apple-touch-icon" href="./media/png/apple-touch-icon.png" />
+        <link rel="stylesheet" href="./static/css/main.css">
+    </head>
+    <body>
