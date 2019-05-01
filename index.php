e409b8d6e0dbd316178da03ab6a477acbf197766
david reinfurt <reinfurt@o-r-g.com>
2019-05-01 17:55:03 -0400

    adjust $msg

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once('./views/temp.php');
 // assemble $msg
 $msg  = 'New York Consolidated . . . ';
 $msg .= $now;
-$msg .= $output['wind_string'];
+$msg .= ' currently ' . $output['wind_string'];
 $msg .= ' /// Currently ' . $output['temp_f'] . ' degrees.';
 $msg .= ' 0 1 2 3 4 5 6 7 8 9 Have a nice day.';
 ?>
